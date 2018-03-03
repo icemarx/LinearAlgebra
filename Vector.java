1cec21dec842d732a2ce39a664fdd3306309adee
icemarx <matjaz.ciglic.jst@gmail.com>
2018-03-03 17:40:04 +0100

    Added vector addition (sum()) and methods rk() and getComponent(), for future use.

--- Vector.java
+++ Vector.java
@@ -7,6 +7,30 @@ class Vector {
     public Vector(double... components) {
         this.components = components;
     }
+
+    public int rk() {     // rk stands for rank
+        // returns the rank of the Vector this
+        return this.components.length;
+    }
+    public double getComponent(int i) {
+        // returns the i-th component of Vector this
+        return this.components[i];
+
+    }
+
+    public Vector sum(Vector a) {
+        // adds the Vectors a and returns the Vector sum, if the ranks of Vectors are the same
+        if(a.rk() == this.components.length) {
+            double[] v = new double[this.components.length];
+            for(int i = 0; i < a.rk(); i++) 
+                v[i] = this.getComponent(i) + a.getComponent(i);
+            return new Vector(v);
+        } else {
+            // will change this later to throw a custom exception
+            return new Vector(0,0,0);
+        }
+    }
+
     
     @Override
     public String toString() {
@@ -16,7 +40,7 @@ class Vector {
                 v+=(int)this.components[i];
             else
                 v+=this.components[i];
-            
+
             if(i != this.components.length - 1)
                 v+="\n";
         }
@@ -25,6 +49,10 @@ class Vector {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Vector(1,2,3));
+        // for testing purposes
+        System.out.println(new Vector(1, 2, 3));
+        System.out.println(new Vector(1, 2.3232, -3.5));
+        System.out.println((new Vector(1, 2, 3)).sum(new Vector(-2, -4, -6)));
+        System.out.println((new Vector(1)).sum(new Vector(1, 2)));      // issue
     }
 }
